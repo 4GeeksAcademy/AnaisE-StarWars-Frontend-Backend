ea11b7a09dc54ed6d579e4563e7a027cc9936e36
Anais Escalona <115326780+eanais@users.noreply.github.com>
2025-06-11 02:10:27 +0000

    desarrollo de la base de datos

--- src/api/models.py
+++ src/api/models.py
@@ -3,10 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), unique=True, nullable=False)
+    password = Column(String(250), nullable=False)
+    email = Column(String(100), unique=True, nullable=True)
+    created = Column(DateTime, server_default=func.now(), nullable=False)
+    edited = Column(DateTime, server_default=func.now(), onupdate=func.now(), nullable=False)
+    
+    favorites = relationship('Favorite', back_populates='user')
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +21,70 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "created": self.created,
+            "edited": self.edited
             # do not serialize the password, its a security breach
         }
+class Character(db.Model):
+    __tablename__ = 'character'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    eye_color = Column(String(50))
+    gender = Column(String(50))
+    hair_color = Column(String(50))
+    created = Column(DateTime, server_default=func.now(), nullable=False)
+    edited = Column(DateTime, server_default=func.now(), onupdate=func.now(), nullable=False)
+    favorites = relationship('Favorite', back_populates='character')
+
+    def __repr__(self):
+        return f'<Character {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "eye_color": self.eye_color,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+            "created": self.created,
+            "edited": self.edited,
+                    
+        }
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    diameter = Column(Numeric)
+    climate = Column(String(250))
+    terrain = Column(String(250))
+    created = Column(DateTime, server_default=func.now(), nullable=False)
+    edited = Column(DateTime, server_default=func.now(), onupdate=func.now(), nullable=False)
+    favorites = relationship('Favorite', back_populates='planet')
+
+def __repr__(self):
+        return f'<Planet {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            ": self,
+            
+        }
+
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    character_id = Column(Integer, ForeignKey('character.id'), nullable=True)
+    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
+    created = Column(DateTime, server_default=func.now(), nullable=False)
+    user = relationship('User', back_populates='favorites')
+    character = relationship('Character', back_populates='favorites')
+    planet = relationship('Planet', back_populates='favorites')
+   
